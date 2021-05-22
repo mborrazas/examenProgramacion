0470edd3a8f88a010078e27ec80da5edee72b49a
Matias Borrazas <=>
2021-05-22 16:42:47 -0300

    Agrega funcionalidad de guardarDatos en DB

--- formulario.php
+++ formulario.php
@@ -10,43 +10,68 @@ $fechaNacimiento = $_POST['fechaNacimiento'];
 validarDatos($nombre, $apellido, $edad, $telefono, $email, $fechaNacimiento);
 
 
-
-function validarDatos($nombre, $apellido, $edad, $telefono, $email, $fechaNacimiento){
-    if($nombre === ''){
+function validarDatos($nombre, $apellido, $edad, $telefono, $email, $fechaNacimiento)
+{
+    if ($nombre === '') {
         echo 'El nombre esta vacio';
         return false;
     }
-    if($apellido === ''){
+    if ($apellido === '') {
         echo 'El apellido esta vacio';
         return false;
     }
-    if($edad === ''){
+    if ($edad === '') {
         echo 'La edad esta vacia';
         return false;
     }
 
-    if($telefono === ''){
+    if ($telefono === '') {
         echo 'El teléfono esta vacio';
         return false;
     }
 
-    if($email === ''){
+    if ($email === '') {
         echo 'El email esta vacio';
         return false;
     }
 
-    if($fechaNacimiento === ''){
+    if ($fechaNacimiento === '') {
         echo 'La fecha de nacimiento esta vacia';
         return false;
     }
-    
-    if(!is_numeric($edad)){
+
+    if (!is_numeric($edad)) {
         echo 'La edad debe ser un numero.';
         return false;
     }
 
-    if(!is_numeric($telefono)){
+    if (!is_numeric($telefono)) {
         echo 'El teléfono debe ser un numero';
         return false;
     }
+    guardarDatos($nombre, $apellido, $edad, $telefono, $email, $fechaNacimiento);
+}
+
+
+function guardarDatos($nombre, $apellido, $edad, $telefono, $email, $fechaNacimiento)
+{
+    $conexion = new mysqli(
+        'localhost',
+        'usuario',
+        'password',
+        'nombre'
+    );
+
+    $sql = "INSERT INTO personas(nombre, apellido, edad, telefono, email, fechaNacimiento) VALUES(
+        '{$nombre}',
+        '{$apellido}',
+        {$edad},
+        '{$telefono},
+        '{$email},
+        '{$fechaNacimiento}
+        ');";
+    $conexion->query($sql);
+
+    echo 'Datos guardados.';
+    return true;
 }
